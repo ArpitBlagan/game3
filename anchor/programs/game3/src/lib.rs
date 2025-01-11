b56e50517b0b027d907f4fcfd3a844b8bddb2078
Arpit Blagan <blaganarpit@gmail.com>
2025-01-11 14:01:46 +0530

    writtern started transaction that we gonna need :)

--- anchor/programs/game3/src/lib.rs
+++ anchor/programs/game3/src/lib.rs
@@ -1,70 +1,202 @@
 #![allow(clippy::result_large_err)]
 
 use anchor_lang::prelude::*;
-
 declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 
 #[program]
 pub mod game3 {
     use super::*;
+    pub fn createChallenge(ctx:Context<CreateChallenge>,name:String, descritpion:String,
+      entry_fee:u32,token_type:String)->Result<()>{
+        //Logic here
+        let challenge=& mut ctx.accounts.challenge_account;
+        challenge.owner=ctx.accounts.payer.key();
+        challenge.name=name;
+        challenge.descritpion=descritpion;
+        challenge.entry_fee=entry_fee;
+        challenge.token_type=token_type;
+        Ok(())
+    }
+    //Pending ??
+    pub fn createParticipant(ctx:Context<CreateParticipant>,user_name:String,player_id:u32)->Result<()>{
+      let challenge=&mut ctx.accounts.challenge_account;
+      let participant_account=& mut ctx.accounts.participant_account;
+      if challenge.participant1.is_some() && challenge.participant2.is_some() {
+        participant_account.owner = Pubkey::default();
+        participant_account.user_name.clear();
+        participant_account.player_id = 0;
+        participant_account.wins = 0;
+        participant_account.losses = 0;
+        msg!("Participant data has been cleared.");
+        return Err(ErrorCode::ParticipantLimitExceeded.into()); // Custom error code for exceeding participant limit
+    }
 
-  pub fn close(_ctx: Context<CloseGame3>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn decrement(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.game3.count = ctx.accounts.game3.count.checked_sub(1).unwrap();
-    Ok(())
-  }
-
-  pub fn increment(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.game3.count = ctx.accounts.game3.count.checked_add(1).unwrap();
-    Ok(())
-  }
-
-  pub fn initialize(_ctx: Context<InitializeGame3>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-    ctx.accounts.game3.count = value.clone();
-    Ok(())
-  }
+    // Add the participant to the challenge
+    if challenge.participant1.is_none() {
+      
+      participant_account.owner=ctx.accounts.payer.key();
+      participant_account.user_name=user_name;
+      participant_account.player_id=player_id;
+      participant_account.wins=0;
+      participant_account.losses=0;
+        challenge.participant1 = Some(ctx.accounts.payer.key());
+    } else if challenge.participant2.is_none() {
+      participant_account.owner=ctx.accounts.payer.key();
+      participant_account.user_name=user_name;
+      participant_account.player_id=player_id;
+      participant_account.wins=0;
+      participant_account.losses=0;
+        challenge.participant2 = Some(ctx.accounts.payer.key());
+    }
+      //Logic here
+      
+      Ok(())
+    }
+    pub fn updateParticipantInfo(ctx:Context<UpdateStats>,_player_id:String,
+      _user_name:String,typee:String)->Result<()>{
+      //Logic here  
+      let participant_account=&mut ctx.accounts.participant_account;
+      if typee=="win"{
+        participant_account.wins+=1;
+      }
+      else if typee=="loss"{
+        participant_account.losses+=1;
+      }
+      else{
+        return Err(ProgramError::InvalidArgument.into());
+      }
+      Ok(())
+    }
+    pub fn delete_participant(ctx:Context<DeleteParticipant>,participant_id:u32)->Result<()>{
+       //Logic here
+       msg!("Participant is deleted successfully :)");
+       Ok(())
+    }
+    pub fn finishChallenge(ctx:Context<FinishChallenge>,challenge_id:u32)->Result<()>{
+      //Logic here
+      msg!("Challenge is deleted successfully :)");
+      Ok(())
+    }
 }
 
-#[derive(Accounts)]
-pub struct InitializeGame3<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
 
+#[derive(Accounts)]
+#[instruction(challenge_id:u32)]
+pub struct CreateChallenge<'info>{
   #[account(
-  init,
-  space = 8 + Game3::INIT_SPACE,
-  payer = payer
+    init,
+    payer=payer,
+    space=8 + Challenge::INIT_SPACE,
+    seeds=[b"challenge".as_ref(),challenge_id.to_le_bytes().as_ref()],
+    bump
   )]
-  pub game3: Account<'info, Game3>,
-  pub system_program: Program<'info, System>,
-}
-#[derive(Accounts)]
-pub struct CloseGame3<'info> {
+  pub challenge_account:Account<'info,Challenge>,
   #[account(mut)]
-  pub payer: Signer<'info>,
+  pub payer :Signer<'info>,
+  pub system_program:Program<'info,System>
+}
 
+#[derive(Accounts)]
+#[instruction(participant_id:u32)]
+pub struct UpdateStats<'info>{
+  #[account(mut)]
+  pub payer:Signer<'info>,
   #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
+    mut,
+    seeds=[b"participant".as_ref(),participant_id.to_le_bytes().as_ref(),payer.key.as_ref()],
+    bump,
+    realloc = 8 + Participant::INIT_SPACE,
+    realloc::payer = payer, 
+    realloc::zero = true, 
   )]
-  pub game3: Account<'info, Game3>,
+  pub participant_account:Account<'info,Participant>,
+  pub system_program:Program<'info,System>
 }
 
 #[derive(Accounts)]
-pub struct Update<'info> {
+#[instruction(challenge_id:u32)]
+pub struct CreateParticipant<'info>{
   #[account(mut)]
-  pub game3: Account<'info, Game3>,
+  pub payer:Signer<'info>,
+  #[account(
+    init,
+    payer=payer,
+    space=8+Participant::INIT_SPACE,
+    seeds=[b"participant".as_ref(),challenge_id.to_le_bytes().as_ref(),payer.key.as_ref()],
+    bump
+  )]
+  pub participant_account:Account<'info,Participant>,
+  #[
+    account(
+      mut,
+      seeds=[b"challenge".as_ref(),payer.key.as_ref(),challenge_id.to_le_bytes().as_ref()],
+      bump
+    )
+  ]
+  pub challenge_account:Account<'info,Challenge>,
+  pub system_program:Program<'info,System>
+}
+
+#[derive(Accounts)]
+#[instruction(challenge_id:u32)]
+pub struct DeleteParticipant<'info>{
+  #[account(mut)]
+  pub payer:Signer<'info>,
+  #[account(
+    mut,
+    seeds=[b"participant".as_ref(),challenge_id.to_le_bytes().as_ref(),payer.key.as_ref()],
+    bump,
+    close=payer,
+  )]
+  pub participant_account:Account<'info,Participant>,
+  pub system_program:Program<'info,System>
+
+}
+#[derive(Accounts)]
+#[instruction(challenge_id:u32)]
+pub struct FinishChallenge<'info>{
+  #[account(mut)]
+  pub payer:Signer<'info>,
+  #[account(
+    mut,
+    close=payer,
+    seeds=[b"challenge".as_ref(),challenge_id.to_le_bytes().as_ref()],
+    bump,
+    
+  )]
+  pub challenge_account:Account<'info,Challenge>,
+  pub system_program:Program<'info,System>
 }
 
 #[account]
 #[derive(InitSpace)]
-pub struct Game3 {
-  count: u8,
+pub struct Challenge {
+  pub owner: Pubkey,
+  #[max_len(32)]
+  pub name: String,
+  #[max_len(70)]
+  pub descritpion:String,
+  pub entry_fee:u32,
+  #[max_len(15)]
+  pub token_type:String,
+  pub start_at:Option<u32>,
+  pub end_at:Option<u32>,
+  pub participant1: Option<Pubkey>,
+  pub participant2: Option<Pubkey>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Participant{
+  pub owner: Pubkey,
+  #[max_len(50)]
+  pub user_name:String,
+  pub wins:u32,
+  pub losses:u32,
+  pub player_id:u32,
+}
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The challenge already has two participants.")]
+    ParticipantLimitExceeded,
 }
--- anchor/target/types/game3.ts
+++ anchor/target/types/game3.ts
@@ -14,90 +14,46 @@ export type Game3 = {
   },
   "instructions": [
     {
-      "name": "close",
+      "name": "createChallenge",
       "discriminator": [
-        98,
-        165,
-        201,
-        177,
-        108,
-        65,
-        206,
-        96
+        238,
+        243,
+        211,
+        163,
+        179,
+        4,
+        85,
+        64
       ],
       "accounts": [
         {
-          "name": "payer",
+          "name": "challengeAccount",
           "writable": true,
-          "signer": true
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  99,
+                  104,
+                  97,
+                  108,
+                  108,
+                  101,
+                  110,
+                  103,
+                  101
+                ]
+              },
+              {
+                "kind": "arg",
+                "path": "challengeId"
+              }
+            ]
+          }
         },
         {
-          "name": "game3",
-          "writable": true
-        }
-      ],
-      "args": []
-    },
-    {
-      "name": "decrement",
-      "discriminator": [
-        106,
-        227,
-        168,
-        59,
-        248,
-        27,
-        150,
-        101
-      ],
-      "accounts": [
-        {
-          "name": "game3",
-          "writable": true
-        }
-      ],
-      "args": []
-    },
-    {
-      "name": "increment",
-      "discriminator": [
-        11,
-        18,
-        104,
-        9,
-        104,
-        174,
-        59,
-        33
-      ],
-      "accounts": [
-        {
-          "name": "game3",
-          "writable": true
-        }
-      ],
-      "args": []
-    },
-    {
-      "name": "initialize",
-      "discriminator": [
-        175,
-        175,
-        109,
-        31,
-        13,
-        152,
-        155,
-        237
-      ],
-      "accounts": [
-        {
           "name": "payer",
-          "writable": true,
-          "signer": true
-        },
-        {
-          "name": "game3",
           "writable": true,
           "signer": true
         },
@@ -106,58 +62,422 @@ export type Game3 = {
           "address": "11111111111111111111111111111111"
         }
       ],
-      "args": []
+      "args": [
+        {
+          "name": "name",
+          "type": "string"
+        },
+        {
+          "name": "descritpion",
+          "type": "string"
+        },
+        {
+          "name": "entryFee",
+          "type": "u32"
+        },
+        {
+          "name": "tokenType",
+          "type": "string"
+        }
+      ]
     },
     {
-      "name": "set",
+      "name": "createParticipant",
       "discriminator": [
-        198,
-        51,
-        53,
-        241,
-        116,
-        29,
-        126,
-        194
+        206,
+        214,
+        20,
+        200,
+        254,
+        25,
+        244,
+        152
       ],
       "accounts": [
         {
-          "name": "game3",
-          "writable": true
+          "name": "payer",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "participantAccount",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  112,
+                  97,
+                  114,
+                  116,
+                  105,
+                  99,
+                  105,
+                  112,
+                  97,
+                  110,
+                  116
+                ]
+              },
+              {
+                "kind": "arg",
+                "path": "challengeId"
+              },
+              {
+                "kind": "account",
+                "path": "payer"
+              }
+            ]
+          }
+        },
+        {
+          "name": "challengeAccount",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  99,
+                  104,
+                  97,
+                  108,
+                  108,
+                  101,
+                  110,
+                  103,
+                  101
+                ]
+              },
+              {
+                "kind": "account",
+                "path": "payer"
+              },
+              {
+                "kind": "arg",
+                "path": "challengeId"
+              }
+            ]
+          }
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
         }
       ],
       "args": [
         {
-          "name": "value",
-          "type": "u8"
+          "name": "userName",
+          "type": "string"
+        },
+        {
+          "name": "playerId",
+          "type": "u32"
+        }
+      ]
+    },
+    {
+      "name": "deleteParticipant",
+      "discriminator": [
+        12,
+        30,
+        139,
+        106,
+        80,
+        104,
+        147,
+        253
+      ],
+      "accounts": [
+        {
+          "name": "payer",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "participantAccount",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  112,
+                  97,
+                  114,
+                  116,
+                  105,
+                  99,
+                  105,
+                  112,
+                  97,
+                  110,
+                  116
+                ]
+              },
+              {
+                "kind": "arg",
+                "path": "challengeId"
+              },
+              {
+                "kind": "account",
+                "path": "payer"
+              }
+            ]
+          }
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
+        }
+      ],
+      "args": [
+        {
+          "name": "participantId",
+          "type": "u32"
+        }
+      ]
+    },
+    {
+      "name": "finishChallenge",
+      "discriminator": [
+        94,
+        22,
+        255,
+        127,
+        158,
+        199,
+        125,
+        68
+      ],
+      "accounts": [
+        {
+          "name": "payer",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "challengeAccount",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  99,
+                  104,
+                  97,
+                  108,
+                  108,
+                  101,
+                  110,
+                  103,
+                  101
+                ]
+              },
+              {
+                "kind": "arg",
+                "path": "challengeId"
+              }
+            ]
+          }
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
+        }
+      ],
+      "args": [
+        {
+          "name": "challengeId",
+          "type": "u32"
+        }
+      ]
+    },
+    {
+      "name": "updateParticipantInfo",
+      "discriminator": [
+        83,
+        149,
+        143,
+        185,
+        46,
+        238,
+        231,
+        251
+      ],
+      "accounts": [
+        {
+          "name": "payer",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "participantAccount",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  112,
+                  97,
+                  114,
+                  116,
+                  105,
+                  99,
+                  105,
+                  112,
+                  97,
+                  110,
+                  116
+                ]
+              },
+              {
+                "kind": "arg",
+                "path": "participantId"
+              },
+              {
+                "kind": "account",
+                "path": "payer"
+              }
+            ]
+          }
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
+        }
+      ],
+      "args": [
+        {
+          "name": "playerId",
+          "type": "string"
+        },
+        {
+          "name": "userName",
+          "type": "string"
+        },
+        {
+          "name": "typee",
+          "type": "string"
         }
       ]
     }
   ],
   "accounts": [
     {
-      "name": "game3",
+      "name": "challenge",
       "discriminator": [
-        255,
-        176,
-        4,
-        245,
-        188,
-        253,
-        124,
-        25
+        119,
+        250,
+        161,
+        121,
+        119,
+        81,
+        22,
+        208
       ]
+    },
+    {
+      "name": "participant",
+      "discriminator": [
+        32,
+        142,
+        108,
+        79,
+        247,
+        179,
+        54,
+        6
+      ]
+    }
+  ],
+  "errors": [
+    {
+      "code": 6000,
+      "name": "participantLimitExceeded",
+      "msg": "The challenge already has two participants."
     }
   ],
   "types": [
     {
-      "name": "game3",
+      "name": "challenge",
       "type": {
         "kind": "struct",
         "fields": [
           {
-            "name": "count",
-            "type": "u8"
+            "name": "owner",
+            "type": "pubkey"
+          },
+          {
+            "name": "name",
+            "type": "string"
+          },
+          {
+            "name": "descritpion",
+            "type": "string"
+          },
+          {
+            "name": "entryFee",
+            "type": "u32"
+          },
+          {
+            "name": "tokenType",
+            "type": "string"
+          },
+          {
+            "name": "startAt",
+            "type": {
+              "option": "u32"
+            }
+          },
+          {
+            "name": "endAt",
+            "type": {
+              "option": "u32"
+            }
+          },
+          {
+            "name": "participant1",
+            "type": {
+              "option": "pubkey"
+            }
+          },
+          {
+            "name": "participant2",
+            "type": {
+              "option": "pubkey"
+            }
+          }
+        ]
+      }
+    },
+    {
+      "name": "participant",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "owner",
+            "type": "pubkey"
+          },
+          {
+            "name": "userName",
+            "type": "string"
+          },
+          {
+            "name": "wins",
+            "type": "u32"
+          },
+          {
+            "name": "losses",
+            "type": "u32"
+          },
+          {
+            "name": "playerId",
+            "type": "u32"
           }
         ]
       }
